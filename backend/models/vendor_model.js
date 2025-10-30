c91496789a2f7ae9bba99d152bc287e0a67470bc
Nayan Goyal <133009536+Nayan0410@users.noreply.github.com>
2025-10-30 19:54:47 +0530

    Make shopName unique in vendor schema

--- backend/models/vendor_model.js
+++ backend/models/vendor_model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const vendorSchema = new mongoose.Schema(
   {
-    shopName: { type: String, required: true },
+    shopName: { type: String, required: true, unique: true },
     ownerName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true },
